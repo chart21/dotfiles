9f76380d753a5aa4a2c5203ea64ccaf273720ec3
Christopher Harth-Kitzerow <christopher.hk.harth-kitzerow@bmw.de>
2024-09-10 14:13:46 +0200

    updated config

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,2 +1,2 @@
 cd tmux && chmod +x ./install_plugins.sh && ./install_plugins.sh && cd ..
-cd zsh && chmod +x ./download_external.sh && ./download_external.sh && cd ..
+cd zsh && chmod +x ./install_plugins.sh && ./install_plugins.sh && cd ..
